16c6b3e322a11ab60728216dcd076c188d373477
Samuel Beaulieu <sambeau@bu.edu>
2018-03-11 22:16:42 -0400

    Adding basic css, html, and js

---
+++ root/js/facebook.js
@@ -0,0 +1,28 @@
+window.fbAsyncInit = function() {
+  FB.init({
+    appId      : '195418547899109',
+    cookie     : true,
+    xfbml      : true,
+    version    : '1.0'
+  });
+  FB.AppEvents.logPageView();
+};
+
+(function(d, s, id){
+   var js, fjs = d.getElementsByTagName(s)[0];
+   if (d.getElementById(id)) {return;}
+   js = d.createElement(s); js.id = id;
+   js.src = "https://connect.facebook.net/en_US/sdk.js";
+   fjs.parentNode.insertBefore(js, fjs);
+ }(document, 'script', 'facebook-jssdk'));
+
+
+FB.getLoginStatus(function(response) {
+    statusChangeCallback(response);
+});
+
+function checkLoginState() {
+  FB.getLoginStatus(function(response) {
+    statusChangeCallback(response);
+  });
+}
