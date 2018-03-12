ac22d634b808ccca2c63a8ff369489a852a96595
Samuel Beaulieu <sambeau@bu.edu>
2018-03-12 11:48:00 -0400

    Fixed facebook button for login w/ facebook.

--- root/js/facebook.js
+++ root/js/facebook.js
@@ -6,20 +6,19 @@ window.fbAsyncInit = function() {
     version    : 'v1.0'
   });
   FB.AppEvents.logPageView();
+
+  FB.getLoginStatus(function(response) {
+      statusChangeCallback(response);
+  });
 };
 
 (function(d, s, id){
    var js, fjs = d.getElementsByTagName(s)[0];
    if (d.getElementById(id)) {return;}
    js = d.createElement(s); js.id = id;
-   js.src = "https://connect.facebook.net/en_US/sdk.js";
+   js.src = 'https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.12&appId=195418547899109&autoLogAppEvents=1';
    fjs.parentNode.insertBefore(js, fjs);
  }(document, 'script', 'facebook-jssdk'));
-
-
-FB.getLoginStatus(function(response) {
-    statusChangeCallback(response);
-});
 
 function checkLoginState() {
   FB.getLoginStatus(function(response) {
@@ -28,5 +27,5 @@ function checkLoginState() {
 }
 
 function statusChangeCallback() {
-  
+
 }
